02e4cf57a18f2798345aebee0216b6af55c69a99
Chris Sison <chris.a.sison@outlook.com>
2020-10-09 07:52:41 -0700

    client-side js init

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+const searchBar = document.getElementById('locationAddress');
+const searchBtn = document.getElementById('searchBtn');
+const address = document.querySelector('.location');
+const forecast = document.querySelector('.weatherForecast');
+const weatherIcon = document.querySelector('.weatherIcon');
+
+function getInfo() {
+    fetch('http://localhost:3000/weather?address=' + searchBar.value).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            }
+            console.log(data);
+            address.textContent = data.forecast;
+            forecast.textContent = data.weatherData;
+            // weatherIcon.textContent = data.icons;
+        })
+    })
+}
+
+searchBtn.addEventListener('click', (e) => {
+    if (searchBar.value === '') {
+        return console.log('Please enter a location.');
+    }
+    getInfo()
+})
+
+searchBar.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && searchBar.value) {
+        getInfo();
+    }
+})
+
+
